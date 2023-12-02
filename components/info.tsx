d778a629ae0858f4095b8e73a51a1c36ea04a231
Jerome Doran <disneyfan9184@gmail.com>
2023-12-02 09:00:31 -0500

    added add to cart functionality to info component

--- components/info.tsx
+++ components/info.tsx
@@ -4,12 +4,22 @@ import { Product } from '@/types';
 import Currency from './ui/currency';
 import Button from './ui/button';
 import { ShoppingCart } from 'lucide-react';
+import useCart from '@/hooks/use-cart';
+import { MouseEventHandler } from 'react';
 
 type InfoProps = {
   data: Product;
 };
 
 export default function Info({ data }: InfoProps) {
+  const cart = useCart();
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className='text-3xl font-bold text-gray-900'>{data.name}</h1>
@@ -33,7 +43,7 @@ export default function Info({ data }: InfoProps) {
         </div>
       </div>
       <div className='mt-10 flex items-center gap-x-3'>
-        <Button className='flex items-center gap-x-2'>
+        <Button className='flex items-center gap-x-2' onClick={onAddToCart}>
           Add To Cart
           <ShoppingCart />
         </Button>
